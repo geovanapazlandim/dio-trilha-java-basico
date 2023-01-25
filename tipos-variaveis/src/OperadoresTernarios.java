6e843613234bf55291fded5f277cbc037de52c21
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-25 16:31:41 -0300

    Exemplo de como funciona operador ternario

---
+++ tipos-variaveis/src/OperadoresTernarios.java
@@ -0,0 +1,10 @@
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 6;
+
+        String resultado = a == b ?"Verdadeiro":"falso";
+
+        System.out.println(resultado);
+    }    
+}
