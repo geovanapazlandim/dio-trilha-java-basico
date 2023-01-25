126bab1b365e4aafc67a47c9495af8c6bbd436fa
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-25 12:55:28 -0300

    forma de concatenar com +

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+    public static void main(String[] args){
+        String concatenacao = "?";
+
+            System.out.print(concatenacao);
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+            System.out.print(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+            System.out.print(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+            System.out.print(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+            System.out.print(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        
+    }
+}
