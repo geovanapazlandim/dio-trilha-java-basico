9fded7565a8aa0bf3eeb8fafbfbb680ec56a3636
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-25 13:05:30 -0300

    torna numero positivo negativo e depois positivo

---
+++ tipos-variaveis/src/OperadoresUniarios.java
@@ -0,0 +1,12 @@
+public class OperadoresUniarios {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = - numero;
+        System.out.println(numero);
+    } 
+}
