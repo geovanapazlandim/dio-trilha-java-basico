b6f35197497e369ecf8f9a6d55719b9e99726f0f
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-25 13:16:02 -0300

    incremento decremento e booleano

--- tipos-variaveis/src/OperadoresUniarios.java
+++ tipos-variaveis/src/OperadoresUniarios.java
@@ -1,12 +1,14 @@
 public class OperadoresUniarios {
     public static void main(String[] args) {
         int numero = 5;
+        //numero++;
+        System.out.println(++ numero);
+        System.out.println(-- numero);
 
-        numero = - numero;
-
-        System.out.println(numero);
-
-        numero = - numero;
-        System.out.println(numero);
+        boolean variavel = true;
+        System.out.println(!variavel);
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
     } 
 }
