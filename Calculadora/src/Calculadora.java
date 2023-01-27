1a4b35c3b6381be70fcc35962583a33e5f1e0182
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-27 11:59:06 -0300

    observação sobre comentários

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * Calculadora
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * Note: leia atentamente a documentação destas classes
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Geovana Paz Landim
+ * @version 1.0
+ * @since 27/01/2023
+ * 
+*/
+public class Calculadora {
+    public static void main(String[] args) throws Exception {
+        /**
+        Este método é utilizado para somar dois números inteiros
+        @param numeroUm este é o primeiro parâmetro do método
+        @param numeroDois este é o segundo parâmetro do método
+        @return int o resultado deste método é a soma dos dois números
+        */
+        
+        
+    }
+    public int somar (int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ Calculadora/src/Comentario.java
@@ -0,0 +1,23 @@
+public class Comentario {
+    public static void main(String[] args) {
+        //oi, eu sou o comentário em uma única linha.
+        /*OI
+         * eu
+         * sou 
+         * um 
+         * comentario
+         * para 
+         * várias
+         * linhas
+         */
+    }
+    /**
+     * esse comentário com duas estrelinhas é para identificar um comentário a nível de documentação
+     * chique, realmente funciona só passar o mouse em cima do nome do método e aparece o comentário, muito chique.
+     * NÃO É PARA USAR DENTRO DE UM BLOCO DE MÉTODO!!!
+     */
+    public void metodo() {
+        
+    }
+    // claramente não é para utilizar comentários para fazer gambiarras e se desviar das boas práticas de programação!!!
+}
