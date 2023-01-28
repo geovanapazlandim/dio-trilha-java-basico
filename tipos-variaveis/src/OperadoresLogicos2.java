89b6334f64b949e90c3335b7f3634d78e5c83f52
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-28 14:11:18 -0300

    Uso de operadores lógicos

---
+++ tipos-variaveis/src/OperadoresLogicos2.java
@@ -0,0 +1,33 @@
+public class OperadoresLogicos2 {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+
+        System.out.println("Conjunção");
+        System.out.println("b1 && b2 " + ( b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+        
+        System.out.println("Disjunção");
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
+
+        System.out.println("Disjunção exclusiva");
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1" + (b4 ^ b1));
+        
+        System.out.println("Negação");
+        System.out.println("!b1" + (!b1));
+        System.out.println("!b1" + (!b1));
+
+        System.out.println("Realizando expressões com os operadores lógicos");
+        System.out.println("(i1 > i2 ) || (f2 < f1) -> " + ((i1 > i2 ) || (f2 < f1)));
+        System.out.println("((i1 + i2) < (f2 - f1)) && true -> " + (((i1 + i2) < (f2 - f1)) && true));
+
+    }
+}
